5d8af2917b1fbc38c5a247113162fb12ff817dc7
mfidalgo <mfidalgo@cloudflare.com>
2023-02-12 22:56:26 +0000

    update readme and styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
-import './App.css';
-import CharactersList from './pages/CharactersList';
+import "./App.css";
+import CharactersList from "./pages/CharactersList";
 import Character from "./pages/Character";
 import { Route, Routes } from "react-router";
 
 function App() {
   return (
     <div className="App">
+      <h1>React With GraphQL (Apollo Client)</h1>
       <Routes>
         <Route strict exact path="/" element={<CharactersList />} />
         <Route strict exact path="/:id" element={<Character />} />
